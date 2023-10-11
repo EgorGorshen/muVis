3cd8d27ff225e7a66985e004e8690eb309df31a0
EgorGorshen <egorgorshenckov@yandex.ru>
2023-10-12 00:20:04 +0300

    feat(init): init base project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-muzloVis
+# muzloVis
+
+## questions
+
+- Как устанавливать си/си++ библиотеки 
+
+## links
+
+- [Youtube vide with this project](https://www.youtube.com/watch?v=Xdbk1Pr5WXU&list=PLpM-Dvs8t0Vak1rrE2NJn8XYEJ5M7-BqT&index=1) 
+- 
---
+++ main.c
@@ -0,0 +1,23 @@
+#include "lib/raylib.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    InitWindow(800, 600, "Hello");
+    SetTargetFPS(60);
+
+    InitAudioDevice();
+    Music music = LoadMusicStream("/Users/gorsenkovegor/Downloads/mp3/Nirvana.mp3");
+    PlayMusicStream(music);
+    while (!WindowShouldClose())
+    {
+        UpdateMusicStream(music);
+
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
+    }
+    return 0;
+}
